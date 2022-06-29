6e5ee7b4780a152ab7bf80ff982d4898f58bcbc4
hafizip00 <hafizip00@gmail.com>
2022-06-30 02:20:46 +0500

    FIRST

---
+++ src/componets/Veggie.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Veggie() {
+  return <div>Veggie</div>;
+}
+
+export default Veggie;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Veggie from "../componets/Veggie";
+import Populor from "../componets/Populor";
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <Veggie />
+      <Populor />
+    </div>
+  );
+}
+
+export default Home;
